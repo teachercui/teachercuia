a70431b8313af0062c2307e9e208c3e80f938359
cuihangtian <321447677@qq.com>
2020-11-16 14:06:42 +0800

    a

--- dingding/dingding.py
+++ dingding/dingding.py
@@ -4,7 +4,7 @@ import requests
 import datetime
 
 
-def send(value):
+#def send(value):
     url = "https://oapi.dingtalk.com/robot/send?access_token=2f3ffa25f4d079a19b09820ffe15fe87f0eed9c0211153eb6d129fb795c9c8e3"
     now_time = datetime.datetime.now().strftime('%Y-%m-%d')
     data = {
@@ -19,7 +19,7 @@ def send(value):
     print(r.text)
 
 
-def getmessage():
+#def getmessage():
     message = []
     url = 'https://it.sohu.com/?spm=smpc.content.nav.9.1584602783660uIFWpoR'
 
@@ -27,12 +27,4 @@ def getmessage():
     import re
     # 正则：匹配换行模式，re.DNTALL
     # 正则：懒惰模式?
-    s = re.findall('(?s)(z-c-block-list-item-first">.*?target="_blank">)(.*?)(</a>)', r.text, re.DOTALL)
-    for x in s:
-        message.append(str(s.index(x) + 1) + '.' + x[1])
-    return '\n'.join(message)
-
-
-if __name__ == '__main__':
-    message = getmessage()
-    send(message)
+    s = re.findall('(?s)(z-c-block-list-item-first">.*?target="_blank">)(.*?)(</a>)', r.text, ge)
